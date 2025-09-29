85292cc1373fc06e22ec535f836370179046ba52
Marcelly Maia <cecellymaia@hotmail.com>
2025-09-29 19:21:36 -0300

    customer_address api

--- codigo-fonte/impressa-digital/backend/src/modules/customer_address/customer_address.controller.ts
+++ codigo-fonte/impressa-digital/backend/src/modules/customer_address/customer_address.controller.ts
@@ -1,8 +1,37 @@
-/*
-https://docs.nestjs.com/controllers#controllers
-*/
+import { Controller, Get, Post, Put, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { CustomerAddress } from 'src/core/database/entities/customer_address.entity';
+import { CustomerAddressService } from './customer_address.service';
 
-import { Controller } from '@nestjs/common';
+@Controller('customer_address')
+export class CustomerAddressController {
+  constructor(private readonly addressService: CustomerAddressService) {}
 
-@Controller()
-export class Customer_addressController {}
+  @Get()
+  async findAll(): Promise<CustomerAddress[]> {
+    return this.addressService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<CustomerAddress> {
+    return this.addressService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() data: Partial<CustomerAddress>): Promise<CustomerAddress> {
+    return this.addressService.create(data);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: Partial<CustomerAddress>,
+  ): Promise<CustomerAddress> {
+    return this.addressService.update(id, data);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<{ message: string }> {
+    await this.addressService.remove(id);
+    return { message: `Endereço ${id} removido com sucesso` };
+  }
+}
