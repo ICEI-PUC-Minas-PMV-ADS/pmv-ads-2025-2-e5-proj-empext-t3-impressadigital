7e5568010434c785d4ac682ef7837c65c0581c92
Marcelly Maia <cecellymaia@hotmail.com>
2025-11-09 19:37:53 -0300

    ajuste no cors do backend

--- codigo-fonte/impressa-digital/backend/src/main.ts
+++ codigo-fonte/impressa-digital/backend/src/main.ts
@@ -25,6 +25,7 @@ async function bootstrap() {
       'http://localhost:3001',
       'http://localhost:8081',
       'http://localhost:19006',
+      'https://impressadigital.pages.dev',
     ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
